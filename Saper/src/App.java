69524126650064f8d0f035f234cd99c8a6bcc9d0
Jey0204 <130754053+Jey0204@users.noreply.github.com>
2024-03-14 13:20:28 +0100

    Delete Saper directory

--- Saper/src/App.java
+++
@@ -1,9 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        Wyswietlanie ok = new Wyswietlanie();
-
-        ok.setDefaultCloseOperation(3);
-        
-        ok.setVisible(true);
-    }
-}
