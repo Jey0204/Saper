061f939e478c67252b5bddfbdde1adfdbdc277d2
jey0204 <ja303143@student.polsl.pl>
2024-03-14 13:15:28 +0100

    first commit

---
+++ src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Wyswietlanie ok = new Wyswietlanie();
+
+        ok.setDefaultCloseOperation(3);
+        
+        ok.setVisible(true);
+    }
+}
